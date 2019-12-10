4c6df8a53d2d43f4eba3c4eb4b5a3998c37bc3b8
JanisUnCo <janiskip2002@gmail.com>
2019-12-10 08:44:13 +0200

    Update kods.py

--- kods.py
+++ kods.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Kaka ir ok'
 
+@app.route('/cakes')
+def cakes():
+    return 'Tortes ir ok'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
