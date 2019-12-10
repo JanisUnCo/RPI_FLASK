503859880f1f3ed81b890763b0ec1f5ba7b3be6d
JanisUnCo <janiskip2002@gmail.com>
2019-12-10 09:25:51 +0200

    k

--- kods.py
+++ kods.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(13,GPIO.OUT)
-
-
-app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -21,6 +21,25 @@ def cakes():
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('page.html', name=name)
+
+@app.route("/readPin/<pin>")
+def readPin(pin):
+   try:
+      GPIO.setup(int(pin), GPIO.IN)
+      if GPIO.input(int(pin)) == True:
+         response = "Pin number " + pin + " is high!"
+      else:
+         response = "Pin number " + pin + " is low!"
+   except:
+      response = "There was an error reading pin " + pin + "."
+
+   templateData = {
+      'title' : 'Status of Pin' + pin,
+      'response' : response
+      }
+
+   return render_template('pin.html', **templateData)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
