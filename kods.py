b7f845ed9332d24196d58a55a35fae2b8d06cf77
JanisUnCo <janiskip2002@gmail.com>
2019-12-10 08:48:53 +0200

    12312321

--- kods.py
+++ kods.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Kaka ir ok'
+    return render_template('index.html')
 
 @app.route('/cakes')
 def cakes():
