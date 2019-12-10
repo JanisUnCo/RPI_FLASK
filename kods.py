933b970a0ff812006e689000bf651f18e531fdab
JanisUnCo <janiskip2002@gmail.com>
2019-12-10 09:36:24 +0200

    Update kods.py

--- kods.py
+++ kods.py
@@ -5,12 +5,60 @@ app = Flask(__name__)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(13,GPIO.OUT)
+
+pins = {
+   13 : {'name' : 'led1', 'state' : GPIO.LOW},
+   19 : {'name' : 'led2', 'state' : GPIO.LOW}
+   }
+
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.LOW)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
-    GPIO.output(13,GPIO.LOW)
+    for pin in pins:
+        pins[pin]['state'] = GPIO.input(pin)
+
+    templateData = {
+        'pins' : pins
+        }
+
+    return render_template('main.html', **templateData)
+
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+    # Convert the pin from the URL into an integer:
+    changePin = int(changePin)
+    # Get the device name for the pin being changed:
+    deviceName = pins[changePin]['name']
+    # If the action part of the URL is "on," execute the code indented below:
+    if action == "on":
+        # Set the pin high:
+        GPIO.output(changePin, GPIO.HIGH)
+        # Save the status message to be passed into the template:
+        message = "Turned " + deviceName + " on."
+    if action == "off":
+        GPIO.output(changePin, GPIO.LOW)
+        message = "Turned " + deviceName + " off."
+    if action == "toggle":
+        # Read the pin and set it to whatever it isn't (that is, toggle it):
+        GPIO.output(changePin, not GPIO.input(changePin))
+        message = "Toggled " + deviceName + "."
+
+    # For each pin, read the pin state and store it in the pins dictionary:
+    for pin in pins:
+        pins[pin]['state'] = GPIO.input(pin)
+
+    # Along with the pin dictionary, put the message into the template data dictionary:
+    templateData = {
+        'message' : message,
+        'pins' : pins
+    }
+
+    return render_template('main.html', **templateData)
 
 @app.route('/cakes')
 def cakes():
