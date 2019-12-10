c7762adb246d95536b59149cdf31097a57d5d2c6
JanisUnCo <janiskip2002@gmail.com>
2019-12-10 08:52:42 +0200

    adasdas

--- kods.py
+++ kods.py
@@ -8,7 +8,7 @@ def index():
 
 @app.route('/cakes')
 def cakes():
-    return 'Tortes ir ok'
+    return render_template('cakes.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
