7f732c99b56a677ffe731906024eb691289be716
JanisUnCo <janiskip2002@gmail.com>
2019-12-17 09:30:53 +0200

    Update kods.py

--- kods.py
+++ kods.py
@@ -5,60 +5,11 @@ app = Flask(__name__)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
-pins = {
-   13 : {'name' : 'led1', 'state' : GPIO.LOW},
-   19 : {'name' : 'led2', 'state' : GPIO.LOW}
-   }
-
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
-
-
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(19,GPIO.OUT)
 @app.route('/')
 def index():
     return render_template('index.html')
-    for pin in pins:
-        pins[pin]['state'] = GPIO.input(pin)
-
-    templateData = {
-        'pins' : pins
-        }
-
-    return render_template('main.html', **templateData)
-
-@app.route("/<changePin>/<action>")
-def action(changePin, action):
-    # Convert the pin from the URL into an integer:
-    changePin = int(changePin)
-    # Get the device name for the pin being changed:
-    deviceName = pins[changePin]['name']
-    # If the action part of the URL is "on," execute the code indented below:
-    if action == "on":
-        # Set the pin high:
-        GPIO.output(changePin, GPIO.HIGH)
-        # Save the status message to be passed into the template:
-        message = "Turned " + deviceName + " on."
-    if action == "off":
-        GPIO.output(changePin, GPIO.LOW)
-        message = "Turned " + deviceName + " off."
-    if action == "toggle":
-        # Read the pin and set it to whatever it isn't (that is, toggle it):
-        GPIO.output(changePin, not GPIO.input(changePin))
-        message = "Toggled " + deviceName + "."
-
-    # For each pin, read the pin state and store it in the pins dictionary:
-    for pin in pins:
-        pins[pin]['state'] = GPIO.input(pin)
-
-    # Along with the pin dictionary, put the message into the template data dictionary:
-    templateData = {
-        'message' : message,
-        'pins' : pins
-    }
-
-    return render_template('index.html', **templateData)
 
 @app.route('/cakes')
 def cakes():
@@ -69,32 +20,12 @@ def gaisma(vards):
     if vards == "janis":
         GPIO.output(13,GPIO.HIGH)
 
-
     return render_template('gaisma.html')
 
 
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('page.html', name=name)
-
-@app.route("/readPin/<pin>")
-def readPin(pin):
-   try:
-      GPIO.setup(int(pin), GPIO.IN)
-      if GPIO.input(int(pin)) == True:
-         response = "Pin number " + pin + " is high!"
-      else:
-         response = "Pin number " + pin + " is low!"
-   except:
-      response = "There was an error reading pin " + pin + "."
-
-   templateData = {
-      'title' : 'Status of Pin' + pin,
-      'response' : response
-      }
-
-   return render_template('pin.html', **templateData)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
